321e6ffcfc4809e3bdb537e4fc12cf467ce8d444
Alban Afmeti <albanafmeti@gmail.com>
2017-11-10 11:43:31 +0100

    updated route service provider

--- back-end/app/Providers/RouteServiceProvider.php
+++ back-end/app/Providers/RouteServiceProvider.php
@@ -52,8 +52,8 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapWebRoutes()
     {
         Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
+            ->namespace($this->namespace)
+            ->group(base_path('routes/web.php'));
     }
 
     /**
@@ -65,9 +65,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+        // I have remove prefix('api') because `api` prefix is added by my web server configuration.
+        Route::middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
     }
 }
