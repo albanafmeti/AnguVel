cbf84bea590a80021cdd85e924b57ed85df3ca94
Alban Afmeti <albanafmeti@gmail.com>
2017-11-10 12:45:23 +0100

    changed environment vars

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  appUrl: 'http://api.terejat.al/',
-  apiUrl: 'http://api.terejat.al/api/v1/',
+  appUrl: 'http://terejat.al/api/',
+  apiUrl: 'http://terejat.al/api/v1/',
   client_id: '1',
   client_secret: 'yz5Smhg0HS3GSB0pDJsurttniFJysIF5Skx9gkn5'
 };
