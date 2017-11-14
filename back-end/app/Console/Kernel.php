b74e7c1ea17e9b16b1ef9a807fabbecf73e38021
Alban Afmeti <albanafmeti@gmail.com>
2017-11-14 22:07:31 +0100

    command updated

--- back-end/app/Console/Kernel.php
+++ back-end/app/Console/Kernel.php
@@ -27,8 +27,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('fetcher:import')->everyMinute();
         $schedule->command('fetcher:xing-random')->everyFiveMinutes();
-        $schedule->command('fetcher:import')->everyTenMinutes();
     }
 
     /**
